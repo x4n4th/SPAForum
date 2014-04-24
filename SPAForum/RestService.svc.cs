83d5f03e2634c0657fa03794c259293818a15a0d
x4n4th <djlafey@gmail.com>
2014-04-24 10:12:15 -0400

    added sand castle comments

--- SPAForum/RestService.svc.cs
+++ SPAForum/RestService.svc.cs
@@ -11,6 +11,11 @@ namespace SPAForum
     // NOTE: In order to launch WCF Test Client for testing this service, please select RestService.svc or RestService.svc.cs at the Solution Explorer and start debugging.
     public class RestService : IRestService
     {
+        /// <summary>
+        /// Gets posts given a certain topic
+        /// </summary>
+        /// <param name="topicId">int topic number</param>
+        /// <returns>array of posts</returns>
         public post[] getPosts(int topicId){
             using(ist331Entities entities = new ist331Entities()){
                 var posts = from s in entities.posts.Where(x=>x.topic_id == topicId) select s;
@@ -18,6 +23,11 @@ namespace SPAForum
             }
         }
 
+        /// <summary>
+        /// Gets Topics given a forumid
+        /// </summary>
+        /// <param name="forumId">forum id</param>
+        /// <returns>an array of topics</returns>
         public topic[] getTopics(int forumId) {
             using (ist331Entities entities = new ist331Entities()) {
                 var topics = from s in entities.topics.Where(x => x.forum_id == forumId) select s;
@@ -25,6 +35,10 @@ namespace SPAForum
             }
         }
 
+        /// <summary>
+        /// Gets a list of forums
+        /// </summary>
+        /// <returns>returns a list containing all forums</returns>
         public forum[] getForums() {
             using (ist331Entities entities = new ist331Entities()) {
                 var forums = from s in entities.forums select s;
